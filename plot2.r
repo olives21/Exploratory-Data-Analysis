8a022ccb8e6b6f5f90b1abd542b190d35a1dac63
Olivia <olives@gmail.com>
2015-03-07 23:24:07 -0800

    Edit1

--- plot2.r
+++ plot2.r
@@ -1,4 +1,4 @@
-## Getting full dataset
+## Getting the full dataset
 full_data <- read.csv("./Data/household_power_consumption.txt", header=T, sep=';', na.strings="?",
                       nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
 full_data$Date <- as.Date(full_data$Date, format="%d/%m/%Y")
@@ -7,7 +7,7 @@ full_data$Date <- as.Date(full_data$Date, format="%d/%m/%Y")
 data <- subset(full_data, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
 rm(full_data)
 
-## Converting dates
+## Converting the dates
 datetime <- paste(as.Date(data$Date), data$Time)
 data$Datetime <- as.POSIXct(datetime)
 
